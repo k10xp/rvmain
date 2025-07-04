e412df76275c944a4de5446d6a58e0211ae6d88c
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-07-04 18:41:12 +0000

    Spring boot scaffold

---
+++ rvapi/src/main/java/com/c2org/rvapi/RvapiApplication.java
@@ -0,0 +1,11 @@
+package com.c2org.rvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RvapiApplication.class, args);
+	}
+}
---
+++ rvapi/src/main/java/com/c2org/rvapi/api/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.c2org.rvapi.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping
+    public String baseRoute() {
+        return "Welcome";
+    }
+}
